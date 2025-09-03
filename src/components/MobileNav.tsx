0e43b2e4935d61f0356d6805cf86086f571359e5
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-09-03 19:47:32 +0530

    add pricing page inside mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -88,6 +88,16 @@ const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
                     Sign out
                   </Link>
                 </li>
+                <li>
+                  <Link
+                    onClick={() =>
+                      closeOnCurrent('/pricing')
+                    }
+                    className='flex items-center w-full font-semibold'
+                    href='/pricing'>
+                    Pricing
+                  </Link>
+                </li>
               </>
             )}
           </ul>
